af29f8abacb929a300a64742a12697317d2ff3a6
Mariano Beccaria <mariano.beccaria1@gmail.com>
2026-03-02 00:34:35 +0000

    - Modified walmart_store_dim model to remove duplicate entries - renamed schema.yml filels to silver.yml and gold.yml - Added new tests to verify composite unique keys

--- models/silver/walmart_store_dim.sql
+++ models/silver/walmart_store_dim.sql
@@ -1,7 +1,6 @@
-
 {{ config(
-    materialized = 'incremental',
-    unique_key   = 'STORE_ID',
+    materialized         = 'incremental',
+    unique_key           = ['STORE_ID', 'DEPT_ID'],
     incremental_strategy = 'merge'
 ) }}
 
@@ -9,26 +8,30 @@ WITH stores AS (
     SELECT STORE_ID, STORE_TYPE, STORE_SIZE
     FROM {{ source('bronze', 'STORES_RAW') }}
 ),
+
 depts AS (
-    SELECT STORE_ID, DEPT_ID
+    SELECT DISTINCT
+        CAST(STORE_ID AS INT) AS STORE_ID,
+        CAST(DEPT_ID  AS INT) AS DEPT_ID
     FROM {{ source('bronze', 'DEPARTMENT_RAW') }}
 ),
+
 final AS (
-    SELECT 
-        CAST(d.STORE_ID AS INT) AS STORE_ID,
-        CAST(d.DEPT_ID AS INT) AS DEPT_ID, 
-        s.STORE_TYPE, 
+    SELECT
+        d.STORE_ID,
+        d.DEPT_ID,
+        s.STORE_TYPE,
         CAST(s.STORE_SIZE AS INT) AS STORE_SIZE,
-        CURRENT_TIMESTAMP() AS INSERT_DATE,
-        CURRENT_TIMESTAMP() AS UPDATE_DATE
-    FROM depts d 
-    LEFT JOIN stores s ON s.STORE_ID = d.STORE_ID
+        CURRENT_TIMESTAMP()       AS INSERT_DATE,
+        CURRENT_TIMESTAMP()       AS UPDATE_DATE
+    FROM depts d
+    LEFT JOIN stores s ON CAST(s.STORE_ID AS INT) = d.STORE_ID
+
+    -- Deduplicate just in case
+    QUALIFY ROW_NUMBER() OVER (
+        PARTITION BY d.STORE_ID, d.DEPT_ID 
+        ORDER BY d.STORE_ID
+    ) = 1
 )
 
 SELECT * FROM final
-
--- incremantal condition to get new records that are not already
-{% if is_incremental() %}
-    -- this filter will only be applied on an incremental run
-    where (STORE_ID, DEPT_ID) NOT IN ( SELECT STORE_ID, DEPT_ID from {{ this }}) 
-{% endif %}
